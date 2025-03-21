c4aec5816df72d4e46bb2a66416bb432f85ae8b8
lngnyntk <elang.nayantoko@gmail.com>
2025-03-21 15:57:57 +0700

    fix

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById('play');
-    p = document.getElementById('lirik');
+    const p = document.getElementById('lirik');
 
     button.addEventListener("click", playSong);
 
     function playSong() {
         button.style.display = 'none';
         console.log('Song is playing');
+        new Audio('audio.mp4').play()
+
         const lirik = [
             "Aku dah lupa tak ingat lagi",
             "Nama kau pun hilang dari hati",
@@ -21,26 +25,24 @@ document.addEventListener("DOMContentLoaded", () => {
             "Aku move on hidup sendiri",
             "Tak perlu kau aku happy"
         ];
-    
+
         console.log("Aku Dah Lupa");
-    
+        p.innerHTML = "";
+
         (async function () {
             await sleep(1000);
             for (const baris_lagu of lirik) {
+                let span = document.createElement("span");
+                p.appendChild(span);
+
                 for (const huruf of baris_lagu) {
-                    if (huruf === ' ') {
-                        p.innerText = p.innerText + ' ';
-                        continue;
-                    }
-                    // process.stdout.write(huruf);
-                    p.innerText = p.innerText + huruf;
+                    span.textContent += huruf;
                     await sleep(50);
                 }
-                p.innerText = p.innerText + '\n';
-                // console.log(baris_lagu);
+
+                p.appendChild(document.createElement("br"));
                 await sleep(1000);
             }
         })();
     }
 });
-
